5490ae6b43d0d368e2ff2b6c06fedf3d68a68ea5
abc1242 <abc1242@naver.com>
2020-08-14 14:41:07 +0900

    연결된 모든 소켓에 메시지 전송..

--- routes/webSocket.js
+++ routes/webSocket.js
@@ -2,31 +2,42 @@
 const WebSocket = require("ws");
 
 let fs = require('fs');
+const { WSAEWOULDBLOCK } = require("constants");
 let writer = fs.createWriteStream('test.txt');
 
-module.exports = function(_server)
-{
-    const wss = new WebSocket.Server( {server:_server} );
+// module.exports = function(_server)
+// {
+const wss = new WebSocket.Server( {port: 4000} );
+CLIENTS=[];
+
+
+wss.on('connection', function(ws){
+    CLIENTS.push(ws);
     
-    wss.on('connection', function(ws, req){
- 
-        ws.on('message', function(message){
-            console.log(message);
-            ws.send(message);
 
-            writer.write(message+'\n'); //데이터 -> txt
+    console.log('새연결')
 
-        });
-
-        ws.on('error', function(error){
-            console.log("에러" + error);
-        })
-
-        ws.on('close', function(){
-            console.log("연결 끝");
-        })
-        
+    
+    ws.on('message', function(message){
+        console.log('메세지' +message); 
+        //writer.write(message+'\n'); //데이터 -> txt
+        for (var i =0; i<CLIENTS.length;i++){
+            CLIENTS[i].send(message);
+        }
+        //ws.send(message);
     });
 
-}
+    // ws.on('error', function(error){
+    //     console.log("에러" + error);
+    // })
+
+    // ws.on('close', function(){
+    //     console.log("연결 끝");
+    // })
+
+            
+});
+
+
+// }
 
